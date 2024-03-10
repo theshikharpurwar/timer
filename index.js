173236fe23518baf06c9db4b212a48b7df667b2a
Shikhar Purwar <spurwar2002@gmail.com>
2024-03-11 03:45:45 +0530

    add a back button and made the buttons dynamic

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('.js-container');
+
 const stopWatchApp = document.querySelector('.js-Stop-Watch');
 stopWatchApp.addEventListener('click', initializeStopwatch);
 
@@ -23,17 +25,15 @@ function initializeStopwatch() {
         <h2 class="js-timer displayed-time">00:00:00</h2>
         <div class="controls">
             <button class="control js-start-timer">Start</button>
-            <button class="control js-stop-timer">Stop</button>
-            <button class="control js-reset-timer">Reset</button>
         </div>
         <div class="progress-bar">
             <div class="progress"></div>
         </div>
-        <div class="message"></div>`;
+        <div class="message"></div>
+        <button class="button-27 js-back">Back</button>`;
     
     document.querySelector('.js-start-timer').addEventListener('click', startStopwatch);
-    document.querySelector('.js-stop-timer').addEventListener('click', stopStopwatch);
-    document.querySelector('.js-reset-timer').addEventListener('click', resetStopwatch);
+    document.querySelector('.js-back').addEventListener('click', returnToSelection);
 }
 
 function initializeCountdownTimer() {
@@ -50,8 +50,81 @@ function initializeCountdownTimer() {
         <div class="progress-bar">
             <div class="progress"></div>
         </div>
+        <button class="button-27 js-back">Back</button>
     `;
     document.querySelector('.js-start-timer-app').addEventListener('click', startCountdownTimer);
+    document.querySelector('.js-back').addEventListener('click', returnToSelection);
+}
+
+function returnToSelection() {
+    container.innerHTML = `<h1 class="header">Choose from the Following</h1>
+        <div class="controls" style="margin-top: 100px;">
+            <button class="button-27 js-Stop-Watch">Stop-Watch</button>
+            <button class="button-27 js-timer">Timer</button>
+        </div>`;
+    
+    const stopWatchApp = document.querySelector('.js-Stop-Watch');
+    stopWatchApp.addEventListener('click', initializeStopwatch);
+    
+    const timerApp = document.querySelector('.js-timer');
+    timerApp.addEventListener('click', initializeCountdownTimer);
+}
+
+function startStopwatch() {
+    if (!isStopwatchRunning) {
+        isStopwatchRunning = true;
+        stopwatchTimer = setInterval(updateStopwatch, 1000);
+        const controls = document.querySelector('.controls');
+        controls.innerHTML = `
+            <button class="control js-pause-timer">Pause</button>
+            <button class="control js-reset-timer">Reset</button>
+        `;
+        document.querySelector('.js-pause-timer').addEventListener('click', pauseStopwatch);
+        document.querySelector('.js-reset-timer').addEventListener('click', resetStopwatch);
+    }
+}
+
+function stopStopwatch() {
+    clearInterval(stopwatchTimer);
+    isStopwatchRunning = false;
+}
+
+function pauseStopwatch() {
+    clearInterval(stopwatchTimer);
+    isStopwatchRunning = false;
+    const controls = document.querySelector('.controls');
+    controls.innerHTML = `
+        <button class="control js-start-timer">Start</button>
+    `;
+    document.querySelector('.js-start-timer').addEventListener('click', startStopwatch);
+}
+
+function resetStopwatch() {
+    stopStopwatch();
+    stopwatchSeconds = 0;
+    stopwatchMinutes = 0;
+    stopwatchHours = 0;
+    displayTime('.js-timer', stopwatchHours, stopwatchMinutes, stopwatchSeconds);
+    updateProgressBar('.progress', 0);
+    const controls = document.querySelector('.controls');
+    controls.innerHTML = `
+        <button class="control js-start-timer">Start</button>
+    `;
+    document.querySelector('.js-start-timer').addEventListener('click', startStopwatch);
+}
+
+function updateStopwatch() {
+    stopwatchSeconds++;
+    if (stopwatchSeconds === 60) {
+        stopwatchSeconds = 0;
+        stopwatchMinutes++;
+    }
+    if (stopwatchMinutes === 60) {
+        stopwatchMinutes = 0;
+        stopwatchHours++;
+    }
+    displayTime('.js-timer', stopwatchHours, stopwatchMinutes, stopwatchSeconds);
+    updateProgressBar('.progress', getProgress(stopwatchSeconds));
 }
 
 function startCountdownTimer() {
@@ -66,29 +139,38 @@ function startCountdownTimer() {
     countdownSeconds = duration % 60;
     displayTime('.timerDisplay', countdownHours, countdownMinutes, countdownSeconds);
     countdownTimer = setInterval(updateCountdownTimer, 1000);
-    document.querySelector('.container').innerHTML += `
-        <div class="item-center">
-            <button class="js-pause-timer control">Pause</button>
-            <button class="js-reset-timer control">Reset</button>
-        </div>
+    const itemCenter = document.querySelector('.item-center');
+    itemCenter.innerHTML = `
+        <button class="js-pause-timer control">Pause</button>
+        <button class="js-reset-timer control">Reset</button>
     `;
     document.querySelector('.js-pause-timer').addEventListener('click', pauseCountdownTimer);
     document.querySelector('.js-reset-timer').addEventListener('click', resetCountdownTimer);
+    document.querySelector('.js-start-timer-app').remove(); // Remove the "Start" button
 }
 
 function pauseCountdownTimer() {
     clearInterval(countdownTimer);
-    document.querySelector('.container').innerHTML += `
-        <div class="item-center">
-            <button class="js-resume-timer control">Resume</button>
-        </div>
+    isCountdownPaused = true;
+    const itemCenter = document.querySelector('.item-center');
+    itemCenter.innerHTML = `
+        <button class="js-resume-timer control">Resume</button>
+        <button class="js-reset-timer control">Reset</button>
     `;
     document.querySelector('.js-resume-timer').addEventListener('click', resumeCountdownTimer);
+    document.querySelector('.js-reset-timer').addEventListener('click', resetCountdownTimer);
 }
 
 function resumeCountdownTimer() {
+    isCountdownPaused = false;
     countdownTimer = setInterval(updateCountdownTimer, 1000);
-    document.querySelector('.container').removeChild(document.querySelector('.item-center'));
+    const itemCenter = document.querySelector('.item-center');
+    itemCenter.innerHTML = `
+        <button class="js-pause-timer control">Pause</button>
+        <button class="js-reset-timer control">Reset</button>
+    `;
+    document.querySelector('.js-pause-timer').addEventListener('click', pauseCountdownTimer);
+    document.querySelector('.js-reset-timer').addEventListener('click', resetCountdownTimer);
 }
 
 function resetCountdownTimer() {
@@ -99,7 +181,7 @@ function resetCountdownTimer() {
 function updateCountdownTimer() {
     if (countdownSeconds === 0 && countdownMinutes === 0 && countdownHours === 0) {
         clearInterval(countdownTimer);
-        alert('Timer expired!');
+        initializeCountdownTimer(); // Prompt for a new countdown time
     } else {
         countdownSeconds--;
         if (countdownSeconds < 0) {
